f6293e27f2ef2176d824e13442ca3544cc30cf64
cecilia reynoso <cecireynoso@gmail.com>
2023-06-03 18:18:16 -0300

    Agregue metodos a Movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,33 @@
 class MoviesController < ApplicationController
   def index
+    @movies = Movie.all
   end
 
   def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+    @movie = Movie.find(params[:id])
   end
 
   def edit
+    @movie = Movie.find(params[:id])
+  end
+
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :price, :year, :synopsis, :director, :genre)
   end
 end
