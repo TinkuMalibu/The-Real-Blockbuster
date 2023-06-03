a3853aaaff206b92dc74e84aea6a0b3419aa3909
matiasavaca <avaca.matias1@gmail.com>
2023-06-03 19:05:52 -0300

    cambios movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,4 @@
 class Movie < ApplicationRecord
   belongs_to :user
-  validates :movie, :price, :genre, :synopsis, :poster_url, :director, :title, :year, presence: true
+  validates :price, :genre, :synopsis, :poster_url, :director, :title, :year, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.new(first_name: "Aaron", last_name: "Deik", phone_number: "1231331", address: "sfsafsf", email:"aaron@gmail.com",encrypted_password: "123456")
+User.create(first_name: "Aaron", last_name: "Deik", phone_number: "1231331", address: "sfsafsf", email:"aaron@gmail.com",encrypted_password: "123456")
 
-Movie.new(title: "Harry Potter", price: 3000, genre: "Fantastico", synopsis: "Hay poderes", poster_url: "https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2021/01/harry-potter-1.jpg?fit=2500%2C1576&quality=50&strip=all&ssl=1", director: "Rowling", user_id: 1)
+Movie.create(user_id: 1, title: "Harry Potter", price: 3000, genre: "Fantastico", synopsis: "Hay poderes", poster_url: "https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2021/01/harry-potter-1.jpg?fit=2500%2C1576&quality=50&strip=all&ssl=1", director: "Rowling",year: 2010)
