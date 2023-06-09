83fc291d369276fcbf789930bb7c7733e70bfa63
Aaron Deik <aaron.deik.rebora@gmail.com>
2023-06-09 16:29:28 -0400

    search funcionando

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,9 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    if params[:query].present?
+      @movies = @movies.where("title ILIKE ?", "%#{params[:query]}%")
+    end
   end
 
   def new
