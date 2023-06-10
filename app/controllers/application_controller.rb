09b9cd9febe6992c1a936c31be1b4113e20e4418
cecilia reynoso <cecireynoso@gmail.com>
2023-06-10 17:23:52 -0300

    Route del log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,13 @@ class ApplicationController < ActionController::Base
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[firstname lastname address phone_number])
 
+  def after_sign_in_path_for(resource)
+    if (request.referer.include?("peliculas=ok") == true)
+        movies_path
+    else
+      super
+    end
+  end
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: %i[firstname lastname address phone_number])
   end
