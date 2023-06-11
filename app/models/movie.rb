26e7401eb195849df71613d84dbbdf92931a2503
Aaron Deik <aaron.deik.rebora@gmail.com>
2023-06-10 21:03:09 -0400

    cambios variados en footer form new movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,10 @@
 class Movie < ApplicationRecord
+
+  GENRES = %w(Action Animation Comedy Documentary Drama Fantasy Biography Horror Musical Sci-Fi Short Silent Sport Thriller Western Avant-Garde Crime Romance LGBTQ+ TV_Movie Adventure Cult Family History War Mystery Erotica)
+
   belongs_to :user
   has_many :rentals, dependent: :destroy
-  validates :price, :genre, :synopsis, :poster_url, :director, :title, :year, presence: true
-
+  validates :price, :synopsis, :poster_url, :director, :title, :year, presence: true
+  validates :genre, inclusion: {in: GENRES }
   has_one_attached :poster_url
 end
